ba6e394f01a8eafb2ccabd879ea9008f6943f66d
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-16 19:31:30 +0530

    added theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import UserDetailsPage from "./components/UserDetails";
 import { UserProvider } from "./context/userContext";
+import { ThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          {/* Home Page */}
-          <Route path="/" element={<HomePage />} />
+    <ThemeProvider>
+      <UserProvider>
+        <Router>
+          <Routes>
+            {/* Home Page */}
+            <Route path="/" element={<HomePage />} />
 
-          {/* User Details Page */}
-          <Route path="/users/:id" element={<UserDetailsPage />} />
-        </Routes>
-      </Router>
-    </UserProvider>
+            {/* User Details Page */}
+            <Route path="/users/:id" element={<UserDetailsPage />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
