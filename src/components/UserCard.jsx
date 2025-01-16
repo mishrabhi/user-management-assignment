fceb6e4eea86e2e69f5cf3ac54e97ae41772e42a
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-16 19:42:39 +0530

    fixed: color contrast for dark mode

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const UserCard = ({ user }) => {
   return (
-    <div className="p-4 bg-white rounded shadow hover:shadow-lg">
-      <h3>{user.name}</h3>
+    <div className="p-4 border rounded-md shadow-md bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100">
+      <h3 className="text-lg font-bold">{user.name}</h3>
       <p>Email: {user.email}</p>
       <p>City: {user.address.city}</p>
       <Link
