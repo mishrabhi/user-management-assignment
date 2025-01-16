e69c46a46fbd23b3d35f16116f015bb4e85c1757
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-16 19:39:12 +0530

    added dark mode

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import SearchBar from "./SearchBar";
 import UserCard from "./UserCard";
 import { UserContext } from "../context/userContext";
+import Header from "./Header";
 
 const HomePage = () => {
   const {
@@ -18,16 +19,19 @@ const HomePage = () => {
     return <div className="text-center text-red-500 py-4">{error}</div>;
 
   return (
-    <div className="container mx-auto py-8 px-4">
-      <SearchBar
-        query={searchQuery}
-        onSearch={handleSearch}
-        onSort={handleSort}
-      />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filteredUsers.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <Header />
+      <div className="container mx-auto py-8 px-4">
+        <SearchBar
+          query={searchQuery}
+          onSearch={handleSearch}
+          onSort={handleSort}
+        />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredUsers.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </div>
       </div>
     </div>
   );
